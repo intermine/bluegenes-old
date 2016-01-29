8f07d8bf13e4c9bd5a83bcdccf4769e7c0d2bbb9
Yo Yehudi <yochannah@gmail.com>
2016-01-29 12:34:54 +0000

    most recently selected list in list tool gets a selected class. Needs styling

--- src/cljs/bluegenes/tools/chooselist/core.cljs
+++ src/cljs/bluegenes/tools/chooselist/core.cljs
@@ -10,27 +10,45 @@
 (defn get-lists []
   (-> flymine .fetchLists (.then (fn [im-lists]
                                    (reset! lists im-lists)))))
+
+(defn most-recent-state [step-data]
+ "Returns most recent state object"
+ (last (:state step-data)))
+
+(defn is-selected [list state]
+  "Returns true when a list name matches the most recent state (user chosen) list name"
+  (= (.-name list) (:chose (most-recent-state state)))
+  )
+
 (defn list-row []
+  "Generates a single list row with  counts and list type."
   (reagent/create-class
-    {:reagent-render (fn [list responders]
-                        [:div
-                         [:span.badge (.-size list)]
-                         [:span {:on-click (fn []
-                                             ((:append-state responders) {:chose (.-name list)})
-                                             ((:has-something responders) {:service {:root "www.flymine.org/query"}
-                                                                           :data {:format "list"
-                                                                                  :type (.-type list)
-                                                                                  :name (.-name list)}}))}
-                          (.-name list)]
-                         [:span.badge.pull-right (.-type list)]])}))
+    {:reagent-render (fn [list responders state]
+      [:tr {:on-click (fn []
+                         ((:append-state responders) {:chose (.-name list)})
+                         ((:has-something responders) {:service {:root "www.flymine.org/query"}
+                                                       :data {:format "list"
+                                                              :type (.-type list)
+                                                              :name (.-name list)}}))
+            :class (if (is-selected list state)
+                     "selected")}
+        [:td {:class "list-type"}
+         [:span {:class (str "type-" (.-type list))} (.-type list)]]
+        [:td {:class "count"} (.-size list)]
+        [:td {:class "list-name"} (.-name list)]
+       ])}))
 
 (defn ^:export main [step-data responders]
-  (let [nothing nil]
+  "Output a table listing all lists in a mine."
     (reagent/create-class
-      {:reagent-render (fn []
-                         [:div
-                          ; [:h2 (:description step-data) ]
-                          [:div (for [l @lists]
-                                  ^{:key (.-name l)} [list-row l responders])]]
-                         )
-       :component-did-mount get-lists })))
+      {:reagent-render (fn [step-data]
+        [:div
+        ; [:h2 (:description step-data) ]
+          [:table {:class "list-chooser"}
+            [:tr
+              [:td "Type"]
+              [:td "#"]
+              [:td "Name"]]
+            (for [l @lists]
+              ^{:key (.-name l)} [list-row l responders step-data])]])
+       :component-did-mount get-lists }))
