b3202c8c8176c092e9728b80552e0f8a43eaff3a
Yo Yehudi <yochannah@gmail.com>
2016-03-10 16:45:52 +0000

    Keyword search now scrolls down to a selected result. Uses new utility helper method util/layouthelpers/scroll-to-next-step

--- src/cljs/bluegenes/timeline/views.cljs
+++ src/cljs/bluegenes/timeline/views.cljs
@@ -114,7 +114,6 @@
     (reagent/create-class
      {:display-name "dashboard"
       :reagent-render (fn [ids]
-        [:div
          [:div.step-container
           [:div.body.dashboard
            (for [rows (partition-all 3 ids)]
@@ -122,7 +121,7 @@
             (for [id rows]
              ^{:key (str "step-row" id)} [:div.cell ^{:key (str "step-container" id)} [step-container id true]]
               )
-            )]]])})))
+            )]])})))
 
 (defn previous-steps
   "Iterate through the history's structure and create step containers for
