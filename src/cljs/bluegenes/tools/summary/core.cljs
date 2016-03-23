2230cbfb5117bf41b1adf07184d6d814ff942bb2
Yo Yehudi <yochannah@gmail.com>
2016-03-23 12:43:17 +0000

    better summary title

--- src/cljs/bluegenes/tools/summary/core.cljs
+++ src/cljs/bluegenes/tools/summary/core.cljs
@@ -41,19 +41,37 @@
             [response (<! (im/summary-fields {:service service} type id))]
               (results-handler response service type)))))
 
+(defn summary-row [val]
+  "casts :val val to string, to ensure boolean values output on the screen.
+  previously they disappeared like magic."
+  [:div
+   [:dt (clj->js (:name val))] [:dd (str (:val val))]])
+
+(defn summary-title []
+  "outputs the result name if present, or just 'summary' otherwise"
+  [:h5
+   (let [name (:val (:name @search-results))]
+    (cond name (str name " ")))
+    "Summary"])
+
 (defn summary []
-  "Visual output of each of the summary fields returned."
+  "Visual output of each of the summary fields returned. Has default locations at top and bottom for more/less significant fields."
    [:div.summary-fields
-    [:h5 "Results"]
+    [summary-title]
     [:dl
-    (for [[k v] @search-results]
-      (if (im/is-good-result? k v)
-      ^{:key (:name v)}
-      [:div
-       ;;casts :val v to string, to ensure boolean values output on the screen.
-       ;;previously they diappeared like magic.
-       [:dt (clj->js (:name v))] [:dd (str (:val v))]]))
-    ]])
+     ;;output certain fields in preferred locations, if they exist.
+     ;;these fields go at the start (Rachel says)
+      (cond (:name @search-results) [summary-row (:name @search-results)])
+      (cond (:title @search-results) [summary-row (:title @search-results)])
+     ;;output everything else
+      (for [[k v] (dissoc @search-results :abstractText :description :name :title)]
+        (if (im/is-good-result? k v)
+          ^{:key (:name v)}
+          [summary-row v]))
+     ;;these need to go at the end (Rachel says).
+      (cond (:description @search-results) [summary-row (:description @search-results)])
+      (cond (:abstractText @search-results) [summary-row (:abstractText @search-results)])
+     ]])
 
 (defn ^:export preview
   "Render a preview of the tool."
