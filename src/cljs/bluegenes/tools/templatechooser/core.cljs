7fbb7462afb3315f6777bf66ac4577d53deb387b
Joshua Heimbach <joshkh@gmail.com>
2016-06-27 17:38:51 +0100

    WIP: Categorize templates by tags.

--- src/cljs/bluegenes/tools/templatechooser/core.cljs
+++ src/cljs/bluegenes/tools/templatechooser/core.cljs
@@ -11,12 +11,11 @@
       (let [[id query] template
             save-state (-> step-data :api :save-state)]
         [:a.list-group-item
-         {:class (if (= (:state step-data) id) "active")
+         {:class    (if (= (:state step-data) id) "active")
           :on-click (fn [] (save-state id))}
          [:h4.list-group-item-heading
           (last (clojure.string/split (:title query) "-->"))]
          [:p.list-group-item-text (:description query)]]))))
-
 
 (defn templates []
   (fn [step-data]
@@ -25,6 +24,16 @@
             [template {:step-data step-data
                        :template  t}]))))
 
+(defn categories-from-template [[name template-details]]
+  (->> (:tags template-details)
+       (filter (fn [tag] (re-find #"im:aspect:" tag)))
+       (map (fn [tag] (last (clojure.string/split tag #"im:aspect:"))))))
+
+(defn parse-categories [templates]
+  (println "PARSING CATEGORIES")
+  (into [] (distinct) (mapcat categories-from-template templates)))
+
+
 (defn ^:export run
   "This function is called whenever the tool makes a change to its state, or its
   upstream data changes."
@@ -32,6 +41,15 @@
    {:keys [input state cache] :as what-changed}
    {:keys [has-something save-state save-cache] :as api}
    global-cache]
+
+  (if (nil? (:categories (:cache snapshot)))
+    (save-cache (assoc (:cache snapshot) :categories (parse-categories (-> global-cache :templates :flymine)) )))
+
+  ;(let [new-cache (cond-> (:cache snapshot)
+  ;                        ;
+  ;                        (empty? (:categories (:cache snapshot)))
+  ;                        )]
+  ;  (save-cache new-cache))
 
   (if (or (nil? cache) (not (nil? input)))
     (let [runnable    (into {} (h/runnable (-> global-cache :models :flymine)
@@ -64,6 +82,11 @@
                                :payload (get-in snapshot [:cache :runnable (:state snapshot)])}
                     :shortcut "viewtable"})))
 
+(defn menu []
+  (fn [step-data]
+    [:div "MENU"
+     (into [:ul.nav.nav-pills] (map (fn [category] ^{:key (str category)} [:li.active (str category)]) (:categories (:cache step-data))))]))
+
 (defn upstream-data []
   (fn [data]
     [:div (str data)]))
@@ -71,5 +94,7 @@
 (defn ^:export main []
   (reagent/create-class
     {:reagent-render (fn [step-data]
-                       [:div [templates step-data]])}))
+                       [:div
+                        [menu step-data]
+                        [templates step-data]])}))
 
